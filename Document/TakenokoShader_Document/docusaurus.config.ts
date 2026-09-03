144f3b63f73a92cbe39eb50a3cb7a84463b29431
kinakomoti <kinakomoti743@gmail.com>
2026-09-03 23:35:21 +0900

    Add: document

---
+++ Document/TakenokoShader_Document/docusaurus.config.ts
@@ -0,0 +1,75 @@
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: 'Takenoko Shader',
+  tagline: 'Documentation',
+
+  future: {
+    v4: true,
+  },
+
+  url: 'https://your-docusaurus-site.example.com',
+  baseUrl: '/',
+
+  organizationName: 'TakenokoShader',
+  projectName: 'TakenokoShader_Document',
+
+  onBrokenLinks: 'throw',
+
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    image: 'img/docusaurus-social-card.jpg',
+    colorMode: {
+      respectPrefersColorScheme: true,
+    },
+    navbar: {
+      title: 'Takenoko Shader',
+      logo: {
+        alt: 'Takenoko Shader Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Doc',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [],
+      copyright: 'Copyright © 2026 kinankomoti',
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
